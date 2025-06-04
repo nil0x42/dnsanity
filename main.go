49564ff52ce49316c038da1a6ca98846e1d46546
nil0x42 <nil0x42@users.noreply.github.com>
2025-06-04 17:25:04 +0200

    tpl validation: fix PerSrvMaxFailures (len(tpl)+1)

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func validateTemplate(
 		GlobRateLimit:			conf.Opts.GlobRateLimit,
 		// per server
 		PerSrvRateLimit:		conf.Opts.TrustedRateLimit,
-		PerSrvMaxFailures:		len(conf.Template), // never drop Trusted Srvs
+		PerSrvMaxFailures:		len(conf.Template) + 1, // never drop Trusted Srvs
 		// per check
 		PerCheckMaxAttempts:	conf.Opts.TrustedAttempts,
 		// per dns query
